6adc93a629ddc4e6d26729ddb98b226e058fe165
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-06-05 01:17:46 +0900

    fix job

--- job.sh
+++ job.sh
@@ -1,3 +1,3 @@
-#!/bin/env sh
+#!/bin/sh
 
-echo "some_metric 2" | curl --data-binary @- http://prometheus-pushgateway.prometheus.svc.cluster.local:9091/metrics/job/some_job
+echo "some_metric 1" | curl --data-binary @- http://prometheus-pushgateway.prometheus.svc.cluster.local:9091/metrics/job/some_job
